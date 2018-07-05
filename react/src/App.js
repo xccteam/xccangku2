aa8978b982eaf842718fdacf627b857e6c43d6ef
xc <1498456599@qq.com>
2018-07-05 22:19:55 +0800

    估计开关以及

--- react/src/App.js
+++ react/src/App.js
@@ -6,21 +6,6 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-<<<<<<< HEAD
-
-=======
-<<<<<<< HEAD
-=======
-<<<<<<< HEAD
-
-
-=======
-
-       
-
->>>>>>> fe862df89117ead688228f2a1b3305d2c9bd6ad0
->>>>>>> 30d1fe84ab4acb68f2f4d586ff89511289e34a66
->>>>>>> a9b5a981faf9ce8083fc324428247d9d38a24f66
         <div>
           {this.props.children}
         </div>
