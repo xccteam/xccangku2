16fe40a5851bcc3a3fbebfaf9ecb1ce9b5948cad
zlj <1043403535@qq.com>
2018-07-06 11:13:14 +0800

    zlj

---
+++ react/src/components/nl_funny.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import '../css/nl_funny.css';
+
+class Nl_funny extends Component{
+    render(){
+        return(
+            <div>
+                
+            </div>
+        )
+    }
+}
+
+export default  Nl_funny;
